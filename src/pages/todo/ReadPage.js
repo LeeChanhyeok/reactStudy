762eee9332fa39c229861863727a5fd527510dec
chanhyeok <chanhyeok@Chsh-m.local>
2025-01-01 16:59:56 +0900

    Todo complete

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,41 +1,15 @@
-import { useCallback } from "react";
-import { createSearchParams, useSearchParams, useNavigate, useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import ReadComponent from '../../components/todo/ReadComponent';
 
 const ReadPage = () => {
-    
-    const {tno} = useParams()
+  const { tno } = useParams();
 
-    const navigate = useNavigate()
-
-    const [queryParams] = useSearchParams()
-    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
-    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
-   
-    const queryStr = createSearchParams({page,size}).toString()
-    
-    const moveToModify = useCallback((tno) => {
-        navigate({pathname:`/todo/modify/${tno}`,
-                  search: queryStr  
-        })
-    },[tno, page, size])
-
-    const moveToList = useCallback(() => {
-        navigate({pathname:`/todo/list`,
-                  search: queryStr  
-        })
-    },[page, size])
-
-    return (
-        <div className="text-3xl font-extrabold">
-            Todo Read Page Component {tno}
-            <div>
-            <button onClick={() => moveToModify(tno)}>Test Modify</button>
-            <button onClick={() => moveToList()}>Test List</button>
-            </div>
-
-
-        </div>
-    );
-}
+  return (
+    <div className="font-extrabold w-full bg-white mt-6">
+      Todo Read Page Component {tno}
+      <ReadComponent tno={tno}></ReadComponent>
+    </div>
+  );
+};
 
 export default ReadPage;
