3d1979a0f7f3279cf986a8e6ffec998a49197a84
Chan Hyeok Lee <ranya230@gmail.com>
2025-01-28 17:10:47 +0900

    dev: products

---
+++ src/components/products/AddComponent.js
@@ -0,0 +1,11 @@
+const AddComponent = () => {
+    return (
+        <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+            <div className="flex justify-center">
+                <h1>Product Add Component</h1>
+            </div>
+        </div>
+    );
+}
+
+export default AddComponent;
